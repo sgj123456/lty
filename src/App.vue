2e36066740905cf5bc9e52fe68dcf6b43e2255cf
sgj123456 <2586743928@qq.com>
2023-01-10 22:49:56 +0800

    大更新

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<script setup>
+import LrcVue from "./components/Lrc.vue"
+import music from "../public/data/music.json";
+import { onMounted, ref } from "vue";
+const tapclick = (TabsPaneContext, Event) => {
+  audio.value = `data/mp3/${music[TabsPaneContext.index - 1]?.link_url}`
+}
+const audio = ref(undefined)
+const img = ref("data/img/lty(0).png")
+const color = ref(10)
+onMounted(
+  () => {
+    const variation = {
+      img: {
+        "start": 0,
+        "end": 5,
+        "step": 1,
+        "time": 3000,
+        "relapse": true,
+        fun(now) {
+          img.value = `data/img/lty(${now}).png`
+        }
+      },
+      color: {
+        "start": 30,
+        "end": 60,
+        "step": 1,
+        "time": 25,
+        "relapse": true,
+        fun(now) {
+          color.value = now
+        }
+      }
+    }
+    Object.keys(variation).forEach(v => {
+      guodu(variation[v])
+    })
+  }
+)
+function guodu({ start, end, step, time, relapse, fun }) {
+  let now = start
+  if (relapse) {
+    setInterval(() => {
+      fun(now)
+      if (now >= end && step > 0 || now <= start && step < 0) {
+        step = -step
+      }
+      now += step
+    }, time)
+  } else {
+    setInterval(() => {
+      if (now >= end) {
+        now = start
+      }
+      fun(now)
+      now += step
+    }, time)
+  }
+}
+function paneClick() {
+  console.log("demo");
+}
+</script>
+<template>
+  <div id="background"
+    :style="{ position: 'fixed', backgroundImage: `linear-gradient(0deg, #00ffccaa, #66ccff ${color}%, #ee0000aa)`, zIndex: '-10' }">
+  </div>
+  <el-tabs tab-position="left" :style="{ height: 'fit-content' }" @tab-click="tapclick" class="demo-tabs">
+    <el-tab-pane label="主页" class="pane">
+      <h1 style="text-shadow: none;">华风夏韵 洛水天依</h1>
+      <span style="left: -2em !important">
+        <strong style="text-shadow: none;"> <em>——</em> 洛天依诞生<em>10</em>周年纪念</strong>&nbsp&nbsp
+      </span>
+    </el-tab-pane>
+    <el-tab-pane :label="value.name" v-for="(value) of music" class="pane">
+      <LrcVue v-bind="value"></LrcVue>
+    </el-tab-pane>
+  </el-tabs>
+  <img :src="img" style="height: 80%;position: fixed;right: 0;top: 20%;z-index: -1;" />
+  <audio :src="audio" autoplay></audio>
+</template>
+<style>
+.demo-tabs>.el-tabs__content {
+  height: 100%;
+  color: #66ccff;
+  font-weight: 900;
+  text-align: right;
+}
+
+.el-tabs--left .el-tabs__item.is-left {
+  font-weight: 900;
+}
+
+.el-tabs--right .el-tabs__content,
+.el-tabs--left .el-tabs__content {
+  font-size: 1.35rem;
+  font-weight: 900;
+  height: 100%;
+  width: 100%;
+}
+
+.demo-tabs {
+  height: 100% !important;
+  display: flex;
+}
+
+#background {
+  height: 100%;
+  width: 100%
+}
+
+.pane {
+  height: 100% !important;
+}
+</style>
---
+++ src/components/Lrc.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { computed, ref, watch } from 'vue';
+const props = defineProps(["link_lrc"])
+function parseLyric(lyric) {
+    const lrcObj = {
+        ti: '',
+        ar: '',
+        al: '',
+        by: '',
+        offset: '',
+        lrc: []
+    };
+
+    /*
+        [ar:艺人名]
+        [ti:曲名]
+        [al:专辑名]
+        [by:编者（指编辑LRC歌词的人）]
+        [offset:时间补偿值] 其单位是毫秒，正值表示整体提前，负值相反。这是用于总体调整显示快慢的。
+    */
+
+    const lrcArr = lyric.split("\n").filter(function (value) {
+        // 1.通过回车去分割歌词每一行,遍历数组，去除空行空格
+        return value.trim() !== ''
+    }).map(function (value) {
+        // 2.解析歌词
+        const line = parseLyricLine(value.trim())
+        // console.log(line);
+        if (line.type === 'lyric') {
+            lrcObj.lrc.push(line.data)
+        } else {
+            lrcObj[line.type] = line.data
+        }
+        return value.trim();
+    })
+
+    function parseLyricLine(line) {
+        const tiArAlByExp = /^\[(ti|ar|al|by|offset):(.*)\]$/
+        const lyricExp = /^\[(\d{2}):(\d{2}).(\d{2,3})\](.*)/
+        let result
+        if ((result = line.match(tiArAlByExp)) !== null) {
+            return {
+                type: result[1],
+                data: result[2]
+            }
+        } else if ((result = line.match(lyricExp)) !== null) {
+            return {
+                type: 'lyric',
+                data: {
+                    time: {
+                        m: result[1],
+                        s: result[2],
+                        ms: result[3]
+                    },
+                    lyric: result[4].trim()
+                }
+            }
+        }
+    }
+
+    return lrcObj;
+}
+const lrc = ref(null)
+fetch(`data/lrc/${props.link_lrc}`)
+    .then(v => v.text()
+        .then(v => parseLyric(v))
+        .then(v => {
+            lrc.value = v
+        })
+
+    )
+
+</script>
+<template>
+    <ul id="lrc" v-if="lrc">
+        <li v-for="(value, key, index) of lrc.lrc" v-text="value.lyric" :key="index"></li>
+    </ul>
+</template>
+<style scoped>
+ul {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+}
+
+li {
+    text-align: center;
+    width: 100%;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    text-shadow: black 1em 1em 1em, black -1em 1em 1em;
+}
+</style>
